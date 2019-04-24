d892b014400d29390b8ef99a5910f53d2c1db812
HC1058503505 <1058503505@qq.com>
2019-04-24 11:25:27 +0800

    增加清除收藏的提示框

--- lib/pages/me/mine_collections.dart
+++ lib/pages/me/mine_collections.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:wepoems_flutter/models/poem_recommend.dart';
 import 'package:wepoems_flutter/pages/recommand/poem_cell.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:wepoems_flutter/pages/detail/poem_detail.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:io';
 
 class MineCollections extends StatefulWidget {
   @override
@@ -18,9 +19,9 @@ class _MineCollectionsState extends State<MineCollections> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    _scrollController.addListener((){
-
-      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
         _page++;
         _getCollections();
       }
@@ -35,13 +36,12 @@ class _MineCollectionsState extends State<MineCollections> {
     }
     provider.getPoemRecomsPaging(limit: 10, page: _page).then((collectionList) {
       if (collectionList == null) {
-        Fluttertoast.cancel();
-
-        Fluttertoast.showToast(
-          msg: "没有更多数据了",
-          gravity: ToastGravity.CENTER,
-          toastLength: Toast.LENGTH_SHORT
-        );
+//        Fluttertoast.cancel();
+//
+//        Fluttertoast.showToast(
+//            msg: "没有更多数据了",
+//            gravity: ToastGravity.CENTER,
+//            toastLength: Toast.LENGTH_SHORT);
 
         return;
       }
@@ -62,6 +62,14 @@ class _MineCollectionsState extends State<MineCollections> {
     _page = 0;
     _getCollections();
   }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _scrollController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,34 +82,70 @@ class _MineCollectionsState extends State<MineCollections> {
             child: IconButton(
                 icon: Icon(Icons.delete, color: Colors.white),
                 onPressed: () {
-                  PoemRecommendProvider provider =
-                      PoemRecommendProvider.singleton;
-                  provider.deleteAll().then((dynamic) {
-                    Fluttertoast.showToast(
-                        msg: "清除成功",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.CENTER);
+                  if (!Platform.isIOS && !Platform.isMacOS) {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text("温馨提示"),
+                            content: Text("确定清除全部收藏吗？"),
+                            actions: <Widget>[
+                              FlatButton(onPressed: () {}, child: Text("确定")),
+                              FlatButton(onPressed: () {}, child: Text("取消"))
+                            ],
+                          );
+                        });
+                  }
 
-                    setState(() {
-                      _collections.clear();
-                    });
-                  }).catchError((error) {
-                    Fluttertoast.showToast(
-                        msg: "清除失败",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.CENTER);
-                  }).whenComplete(() {});
+                  showCupertinoDialog(
+                      context: context,
+                      builder: (context) {
+                        return CupertinoAlertDialog(
+                          title: Text("温馨提示"),
+                          content: Text("确定清除全部收藏吗？"),
+                          actions: <Widget>[
+                            CupertinoDialogAction(
+                              child: Text(
+                                "取消",
+                                style: TextStyle(
+                                    color: Theme.of(context).primaryColor),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            CupertinoDialogAction(
+                              child: Text(
+                                "确定",
+                                style: TextStyle(
+                                    color: Theme.of(context).primaryColor),
+                              ),
+                              onPressed: () {
+                                PoemRecommendProvider provider =
+                                    PoemRecommendProvider.singleton;
+                                provider.deleteAll().then((dynamic) {
+                                  Navigator.of(context).pop();
+                                  Fluttertoast.showToast(
+                                      msg: "清除成功",
+                                      toastLength: Toast.LENGTH_SHORT,
+                                      gravity: ToastGravity.CENTER);
+
+                                  setState(() {
+                                    _collections.clear();
+                                  });
+                                }).catchError((error) {
+                                  Fluttertoast.showToast(
+                                      msg: "清除失败",
+                                      toastLength: Toast.LENGTH_SHORT,
+                                      gravity: ToastGravity.CENTER);
+                                }).whenComplete(() {});
+                              },
+                            )
+                          ],
+                        );
+                      });
                 }),
           ),
-//          Offstage(
-//            offstage: _collections != null && _collections.length > 0,
-//            child: IconButton(
-//                icon: Icon(Icons.add, color: Colors.white,),
-//                onPressed: (){
-//
-//                }
-//            ),
-//          )
         ],
       ),
       body: collectionsListView(),
@@ -129,13 +173,15 @@ class _MineCollectionsState extends State<MineCollections> {
                 child: GestureDetector(
                   child: PoemCell(poem: _collections[index]),
                   onTap: () {
-                    Navigator.of(context).push(CupertinoPageRoute(builder: (context) {
+                    Navigator.of(context)
+                        .push(CupertinoPageRoute(builder: (context) {
                       return PoemDetail(poemRecom: _collections[index]);
                     }));
                   },
                 ),
                 onDismissed: (direction) {
-                  PoemRecommendProvider provider = PoemRecommendProvider.singleton;
+                  PoemRecommendProvider provider =
+                      PoemRecommendProvider.singleton;
                   provider.delete(_collections[index].idnew).then((dynamic) {
                     Fluttertoast.showToast(
                         msg: "取消收藏成功",
@@ -163,8 +209,7 @@ class _MineCollectionsState extends State<MineCollections> {
             },
             itemCount: _collections == null ? 0 : _collections.length,
           ),
-          onRefresh: _onRefresh
-      ),
+          onRefresh: _onRefresh),
     );
   }
 }
